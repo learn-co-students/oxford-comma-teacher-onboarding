08f4080d2a70b96364e6f8943619e2ac3b8ee861
Alan Hong <ahong2@SF-US04894-MBA.local>
2018-10-03 09:11:08 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  if array.length > 1
+    last_element = array.pop()
+    array << "and #{last_element}"
+  end
 
+  if array.length == 2
+    array.join(" ")
+  else
+    array.join(", ")
+  end
 end
