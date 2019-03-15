4eeb34b50fa8b96a2f02a390b38eed8dbbac4119
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-15 15:21:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1 
+    array[0]
+  elsif array.size == 2 
+    array[0] + " and " + array[1]
+  else
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
 end
